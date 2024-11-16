e6b6c4904560dfc6b7857c4eed490b41b36c5b80
nedarctic <nedarctic@gmail.com>
2024-11-16 21:21:34 +0300

    Added unit tests for creating custom user and custom admin

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+class CustomUserTests(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create(
+            username = 'testuser',
+            email = 'testuser@email.com',
+            password = 'testpass123'
+        )
+        self.assertEqual(user.username, 'testuser')
+        self.assertEqual(user.email, 'testuser@email.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+        
+    def test_create_admin(self):
+        User = get_user_model()
+        admin = User.objects.create_superuser(
+            username = 'admin',
+            email = 'admin@email.com',
+            password = 'adminpass123'
+        )
+        self.assertEqual(admin.username, 'admin')
+        self.assertEqual(admin.email, 'admin@email.com')
+        self.assertTrue(admin.is_active)
+        self.assertTrue(admin.is_staff)
+        self.assertTrue(admin.is_superuser)
